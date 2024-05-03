e7be24eb80502d1fd819ecf679c654607f1e1079
Seerat Mann <sman833@aucklanduni.ac.nz>
2024-05-03 15:35:17 +1200

    Used MessageCli to print START_ROUND message. 2 test cases passing.

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -5,13 +5,16 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 /** This class represents the Game is the main entry point. */
 public class Game {
+  private int round = 1;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
-  public void play() {}
+  public void play() {
+    MessageCli.START_ROUND.printMessage(String.valueOf(round));
+  }
 
   public void endGame() {}
 
